2f2d49e979d1aa09153f17facd4b84e2e0df531d
rakshit-kant <rakshitkant100@gmail.com>
2026-07-08 22:39:46 +0530

    Made the Input Output working loop of the Shell

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
+use std::io::{self, Write};
+
+fn get_string() -> String {
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to Read from Stdin");
+
+    input.truncate(input.trim_end().len());
+
+    input
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello Ash!");
+    loop {
+        print!("rustsh> ");
+        let input = get_string();
+
+        if input == "exit" {
+            break;
+        }
+
+        println!("You typed: {}", input);
+    }
 }
